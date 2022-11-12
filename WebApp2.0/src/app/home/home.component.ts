72441b74003b0c02c9ce1e2306cd0159d8d8d39f
raulmoraru <raul.moraru20@yahoo.com>
2022-11-12 13:32:08 +0200

    created new folder

---
+++ WebApp2.0/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public title = 'AmbrosiaAlert';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
