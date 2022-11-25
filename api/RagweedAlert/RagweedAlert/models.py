1414211ca9fcd5d5aa4ff2ecc38557bbe7e524b6
Ubuntu <azureuser@OnCloud.djk3spnyqhmure2ow4jlyut4id.ax.internal.cloudapp.net>
2022-11-25 13:13:35 +0000

    Delete old API

--- api/RagweedAlert/RagweedAlert/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class RagweedAlert(models.Model):
-    date = models.DateTimeField()
-    location = models.CharField(max_length=500)
--- api/RagweedAlert/RagweedAlert/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import RagweedAlert
-
-class RagweedAlertSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RagweedAlert
-        fields = ['id', 'date', 'location']
