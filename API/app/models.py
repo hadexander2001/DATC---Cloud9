e573371ccf67a7eade94cd78132f26f121c5ca74
Hady Badleh <hadexander2001@gmail.com>
2022-11-25 15:56:12 +0000

    Configured everything to work on container

--- API/app/models.py
+++ API/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Alert(models.Model):
+
+    location = models.CharField(
+        null=False,
+        blank=False,
+        max_length=50
+    )
+
+    time = models.DateTimeField(
+        null=False,
+        blank=False
+    )
