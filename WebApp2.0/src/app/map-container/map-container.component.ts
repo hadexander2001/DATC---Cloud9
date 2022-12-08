56b2b2cb0063c97262b2834eade48a3b2fa9aaa8
raulmoraru <raul.moraru20@yahoo.com>
2022-12-09 01:19:19 +0200

    Created separate functions for the initialization of the map and setting the marker

--- WebApp2.0/src/app/map-container/map-container.component.ts
+++ WebApp2.0/src/app/map-container/map-container.component.ts
@@ -1,56 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 declare const L: any;
+// declare let mymap: any;
+
 @Component({
   selector: 'app-map-container',
   templateUrl: './map-container.component.html',
   styleUrls: ['./map-container.component.scss'],
 })
+
 export class MapContainerComponent implements OnInit {
   public latitude: number | undefined;
   public longitude: number | undefined;
   public markerSettings: object | undefined;
-  public shouldShowMarker = false;
+  public isButtonClicked = false;
 
   constructor() { }
+
   ngOnInit(): void {
-    if (!navigator.geolocation) {
-      return
-    }
+    this.initializeMap();
+  }
+
+  public initializeMap() {
     navigator.geolocation.getCurrentPosition((position) => {
       this.latitude = position.coords.latitude;
       this.longitude = position.coords.longitude;
-      let mymap = L.map('map').setView([this.latitude, this.longitude], 13);
+      let mymap = L.map('map').setView([45.7489, 21.2087], 13) //initialize map
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(mymap);
-
-      var circle = L.circle([this.latitude, this.longitude], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 100
-      }).addTo(mymap);
+      if (this.isButtonClicked) {
+        var circle = L.circle([this.latitude, this.longitude], { //sets marker
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.5,
+          radius: 200
+        }).addTo(mymap);
+      }
     })
-
-    if (this.latitude && this.longitude) {
-      this.setMarker();
-    }
   }
 
   public setMarker() {
-    this.shouldShowMarker = true;
-    this.markerSettings = [{
-      visibility: true,
-      shape: 'Image',
-      imageUrl: 'maps/default-map/datetime/ballon.png',
-      dataSource: [
-        { latitude: this.latitude },
-        { longitude: this.longitude }
-      ],
-      animationDuration: 3
-    }],
-      this.shouldShowMarker = true;
+    this.isButtonClicked = true;
   }
-
 }
