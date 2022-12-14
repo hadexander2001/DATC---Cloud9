6cf8dd19615a5b2e540b7b09a6f895dcb1c8e3d7
gdeeniss <denis.gurita@yahoo.com>
2022-12-14 23:28:21 +0200

    Cron exists

--- API/cron.py
+++ API/cron.py
@@ -3,7 +3,8 @@ import requests
 import datetime
 
 alerts = requests.get('https://ragweedalertsapi.azurewebsites.net/alerts/?format=json')
-
+update = requests.get('https://ragweedalertsapi.azurewebsites.net/alerts')
+update
 alerts_json = json.loads(alerts.text)
 
 current_time = datetime.datetime.now()
@@ -27,4 +28,3 @@ for i in alerts_json:
         print('Old alert that needs deleting')
 
     print(f'---------\n')
-
