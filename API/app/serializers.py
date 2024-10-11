43516da4ac491e68d9ce353b4b9fa71654bfdf24
raulmoraru <118025170+raulmoraru@users.noreply.github.com>
2024-10-11 13:17:58 +0100

    Default Message

--- API/app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Alert
-
-class AlertSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Alert
-        fields = ['id', 'time', 'location_latitude', 'location_longitude']
